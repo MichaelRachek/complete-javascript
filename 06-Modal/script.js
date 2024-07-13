2d5bf464fe7a761e80c082ddc27048e50b00ac49
mrachek <mrachek@asocscloud.com>
2024-07-13 20:10:48 +0300

    added native js modal window

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -5,25 +5,24 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-const openModal = function () {
+const openModal = () => {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
+const closeModal = () => {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
+for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
+}
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
-  // console.log(e.key);
-
+document.addEventListener('keydown',  (e) => {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
