4c60d3b78bed1cf79c59204cf1a948c805df865f
mrachek <mrachek@asocscloud.com>
2024-09-06 21:00:53 +0300

    updated functions section

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,25 @@
-'use strict';
+const oneWord = function(str) {
+  return str.replace(/\s/g, '').toLowerCase();
+}
+
+const upperFirstWord = function(str) {
+  const [first, ...others] = str.split('');
+  return [first.toUpperCase(), ...others].join('');
+}
+
+// Higher-order function when function can get another function as a argument
+const transformer = function(str, fn) {
+  console.log(`transformer: ${fn(str)}`);
+}
+
+transformer('Js the best', upperFirstWord);
+
+// Closer
+
+let f;
+const g = () => {
+  const a = 23;
+  f = () => {
+    console.log(a * 2);
+  }
+}
